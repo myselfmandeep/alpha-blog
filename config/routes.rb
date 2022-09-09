84335b7a0eb53b40a4c369f400547400a837633d
myselfmandeep <mandeeprawat2516@outlook.com>
2022-09-10 02:29:07 +0530

    Add aprtials and destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root "pages#home"
+  get 'about', to: 'pages#about'
   resources :articles
   
 end
